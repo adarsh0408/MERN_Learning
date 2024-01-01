e47195e176afb366b0f67efc8d1a81a4b02fe3c1
Adarsh Shukla <adarsh.04082002@gmail.com>
2024-01-01 20:16:26 +0530

    Routings

--- backend/index.js
+++ backend/index.js
@@ -6,4 +6,4 @@ app.get('/',(req,res)=>{
     res.send("App is running")
 });
 
-app.listen(3000);
+app.listen(5000);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import Nav from './Nav';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-     <h1>E-Dashboard</h1>
+      <BrowserRouter>
+      <Nav/>
+     <Routes>
+      <Route path='/' element={<h1>Product List</h1>}/>
+      <Route path='/add' element={<h1>Add Product</h1>}/>
+      <Route path='/update' element={<h1>Update Product</h1>}/>
+      <Route path='/logout' element={<h1>Logout</h1>}/>
+      <Route path='/profile' element={<h1>Profile</h1>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
